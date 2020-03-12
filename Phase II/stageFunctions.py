7b60b7271746cd4ca8d63576543c842afee92b0b
Paras Goyal <parasgoyal12@gmail.com>
2020-03-12 12:15:35 +0530

    Memory

--- Phase II/stageFunctions.py
+++ Phase II/stageFunctions.py
@@ -1,12 +1,15 @@
 R=[0 for i in range(32)]
 
 MEM={}
-
-IR=''
+PC=0
+MAR=0
+MDR=0
+IR=0
 operand1=0
 operand2=0
 
 def load_program_memory(file_name):
+	#Loads the program and data memory from the mc file
 	try:
 		f=open('./'+file_name)
 		ins=f.readline().split()
@@ -30,9 +33,37 @@ def load_program_memory(file_name):
 	except FileNotFoundError:
 		print("Error opening target code!")
 
-load_program_memory('test.mc')
+def read(address,num_bytes=1):
+	#address assumed to be int with base10
+	#Insert Check Bounds Here
+	#Value returned in Integer Format base10
+	val=0
+	for i in range(num_bytes):
+		adr=address+i
+		if adr in MEM.keys():
+			val=val+MEM[adr]*(1<<(8*i))
+	return val
+		
 
-print(MEM)
+def write(address,data,num_bytes=1):
+	
+	#Address: assumed to be of type integer base10
+	#INPUTS:-Data is assumed to be of type int base 10
+	#Insert Memory Bounds Here
+	#Assuming Byte Addressibility	
+	#num_bytes=len(data)/2-1
+	for i in range(num_bytes):
+		#d_in=int(data[i*(-2)-2:i*(-2)],16)
+		#adr=int(address)+i
+		#MEM[adr]=d_in
+		d_in=(data>>(8*i))&(0xFF)
+		adr=adr+i
+		MEM[adr]=d_in
+		
+
+#load_program_memory('test.mc')
+
+#print(MEM)
 def fetch():
 	pass
 def decode():
@@ -40,31 +71,22 @@ def decode():
 def execute():
 	pass
 def memory_access():
-	pass
+	#Effective address stored in MAR in integer format
+	#Data stored in MDR in Integer Format
+	opcode=IR&(0x7F)
+	funct3=(IR>>12)&(0x7)
+	if opcode == 3:
+		if funct3>=0 and funct3<=3:
+			MDR=read(MAR,2**(funct3))
+	elif opcode== (8*4+3):
+		if funct3>=0 and funct3<=3:
+			write(MAR,MDR,2**(funct3))
+	else:	
+		pass
 def write_back():
 	pass
 
 
-def read(address,num_bytes=1):
-	#Insert Check Bounds Here
-	val=0
-	for i in range(num_bytes):
-		adr=int(address)+i
-		if adr in MEM.keys():
-			val=val+MEM[adr]
-	return val
-		
-
-def write(address,data):
-	#Address: Hex value of 32 bits
-	#INPUTS:-Data is assumed to be of length , atleast 4 and begin with 0x
-	#Insert Memory Bounds Here
-	#Assuming Byte Addressibility	
-	num_bytes=len(data)/2-1
-	for i in range(num_bytes):
-		d_in=int(data[i*(-2)-2:i*(-2)],16)
-		adr=int(address)+i
-		MEM[adr]=d_in
 
 		
 		
