e1a8829548925932491714039740018d61e2ed78
SakshayMahna <sakshum19@gmail.com>
2020-03-13 12:45:58 +0530

    Register and Class Implementation

--- Phase II/stageFunctions.py
+++ Phase II/stageFunctions.py
@@ -1,90 +1,127 @@
-R=[0 for i in range(32)]
+class ProcessingUnit:
+	def __init__(self, file_name):
+		#Initialize the Processing Unit
+		self.RegisterFile = [0 for i in range(32)]
+		self.MEM = {}
+		self.MAR = 0
+		self.MDR = 0
+		self.RZ = 0
+		self.RM = 0
+		self.RY = 0
+		self.PC = 0
+		self.PC_temp = 0
+		self.RA = 0
+		self.RB = 0
+		self.IR = 0
+		self.operand1 = 0
+		self.operand2 = 0
 
-MEM={}
-PC=0
-MAR=0
-MDR=0
-IR=0
-operand1=0
-operand2=0
+		self.load_program_memory(file_name)
 
-def load_program_memory(file_name):
-	#Loads the program and data memory from the mc file
-	try:
-		f=open('./'+file_name)
-		ins=f.readline().split()
-		while ins[1]!='0x00000000':		
-			MEM[int(ins[0],0)]=int(ins[1][-2:],16)
-			MEM[int(ins[0],0)+1]=int(ins[1][-4:-2],16)
-			MEM[int(ins[0],0)+2]=int(ins[1][-6:-4],16)
-			MEM[int(ins[0],0)+3]=int(ins[1][-8:-6],16)
+	def load_program_memory(self, file_name):
+		#Loads the program and data memory from the mc file
+		try:
+			f=open('./'+file_name)
 			ins=f.readline().split()
-		MEM[int(ins[0],0)]=0
-		MEM[int(ins[0],0)+1]=0
-		MEM[int(ins[0],0)+2]=0
-		MEM[int(ins[0],0)+3]=0
-		for l in f:
-			if l=='\n':
-				continue
-			else:
-				l=l.split()
-				MEM[int(l[0],0)]=int(l[1],0)
+			while ins[1]!='0x00000000':		
+				self.MEM[int(ins[0],0)]=int(ins[1][-2:],16)
+				self.MEM[int(ins[0],0)+1]=int(ins[1][-4:-2],16)
+				self.MEM[int(ins[0],0)+2]=int(ins[1][-6:-4],16)
+				self.MEM[int(ins[0],0)+3]=int(ins[1][-8:-6],16)
+				ins=f.readline().split()
+			self.MEM[int(ins[0],0)]=0
+			self.MEM[int(ins[0],0)+1]=0
+			self.MEM[int(ins[0],0)+2]=0
+			self.MEM[int(ins[0],0)+3]=0
+			for l in f:
+				if l=='\n':
+					continue
+				else:
+					l=l.split()
+					self.MEM[int(l[0],0)]=int(l[1],0)
+			
+		except FileNotFoundError:
+			print("Error opening target code!")
 		
-	except FileNotFoundError:
-		print("Error opening target code!")
+	def read(self, address,num_bytes=1):
+		#address assumed to be int with base10
+		#Insert Check Bounds Here
+		#Value returned in Integer Format base10
+		val=0
+		for i in range(num_bytes):
+			adr=address+i
+			if adr in self.MEM.keys():
+				val=val+self.MEM[adr]*(1<<(8*i))
+		return val
+			
 
-def read(address,num_bytes=1):
-	#address assumed to be int with base10
-	#Insert Check Bounds Here
-	#Value returned in Integer Format base10
-	val=0
-	for i in range(num_bytes):
-		adr=address+i
-		if adr in MEM.keys():
-			val=val+MEM[adr]*(1<<(8*i))
-	return val
+	def write(self, address,data,num_bytes=1):
+		#Address: assumed to be of type integer base10
+		#INPUTS:-Data is assumed to be of type int base 10
+		#Insert Memory Bounds Here
+		#Assuming Byte Addressibility	
+		#num_bytes=len(data)/2-1
+		for i in range(num_bytes):
+			#d_in=int(data[i*(-2)-2:i*(-2)],16)
+			#adr=int(address)+i
+			#MEM[adr]=d_in
+			d_in=(data>>(8*i))&(0xFF)
+			adr=adr+i
+			self.MEM[adr]=d_in
 		
-
-def write(address,data,num_bytes=1):
-	
-	#Address: assumed to be of type integer base10
-	#INPUTS:-Data is assumed to be of type int base 10
-	#Insert Memory Bounds Here
-	#Assuming Byte Addressibility	
-	#num_bytes=len(data)/2-1
-	for i in range(num_bytes):
-		#d_in=int(data[i*(-2)-2:i*(-2)],16)
-		#adr=int(address)+i
-		#MEM[adr]=d_in
-		d_in=(data>>(8*i))&(0xFF)
-		adr=adr+i
-		MEM[adr]=d_in
-		
-
-#load_program_memory('test.mc')
-
-#print(MEM)
-def fetch():
-	pass
-def decode():
-	pass
-def execute():
-	pass
-def memory_access():
-	#Effective address stored in MAR in integer format
-	#Data stored in MDR in Integer Format
-	opcode=IR&(0x7F)
-	funct3=(IR>>12)&(0x7)
-	if opcode == 3:
-		if funct3>=0 and funct3<=3:
-			MDR=read(MAR,2**(funct3))
-	elif opcode== (8*4+3):
-		if funct3>=0 and funct3<=3:
-			write(MAR,MDR,2**(funct3))
-	else:	
+	def fetch(self):
 		pass
-def write_back():
-	pass
+
+
+	def decode(self):
+		opcode = self.IR&(0x7F)
+		#U and UJ format
+		if(opcode == 23 or opcode == 55 or opcode == 111):
+			pass
+		#I format
+		elif(opcode == 3 or opcode == 19 or opcode == 103):
+			rs1 = self.IR&(0xF8000)
+			rs1 = rs1 << 15
+			self.RA = self.RegisterFile[rs1]
+		#R S SB format
+		else:
+			rs1 = self.IR&(0xF8000)
+			rs1 = rs1 << 15
+			rs2 = self.IR&(0x1F0000)
+			rs2 = rs2 << 20
+			self.RA = self.RegisterFile[rs1]
+			self.RB = self.RegisterFile[rs2]
+
+	def execute(self):
+		pass
+
+	def memory_access(self):
+		#Effective address stored in MAR in integer format
+		#Data stored in MDR in Integer Format
+		opcode=self.IR&(0x7F)
+		funct3=(self.IR>>12)&(0x7)
+		if opcode == 3:
+			if funct3>=0 and funct3<=3:
+				self.MDR=self.read(self.MAR,2**(funct3))
+		elif opcode== (8*4+3):
+			if funct3>=0 and funct3<=3:
+				self.write(self.MAR,self.MDR,2**(funct3))
+		else:	
+			pass
+
+	def write_back(self):
+		#Determine whether write back is used
+		opcode = self.IR&(0x7F)
+		if opcode == 35 or opcode == 99:
+			pass
+		
+		rd = self.IR&(0xF80)
+		rd = rd/128
+
+		self.RegisterFile[rd] = self.RY
+
+		return
+
 
 
 
