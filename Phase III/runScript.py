60a4c7c88de3bd3963f271e0b5a89dcaa9abdcc4
SakshayMahna <sakshum19@gmail.com>
2020-04-17 18:43:04 +0530

    Different States between different instructions

--- Phase III/runScript.py
+++ Phase III/runScript.py
@@ -7,18 +7,26 @@ else:
 proc=ProcessingUnit(f'{f_name}')
 in_states=[]
 out_states=[]
+
+# States
 state = State()
+IF_ID = State()
+ID_EX = State()
+EX_MEM = State()
+MEM_WB = State()
+
 print('Loaded program in Memory!')
 while True:
 	print(f'Processing Instruction at {hex(state.PC)}')
-	state = proc.fetch(state)
+	IF_ID = proc.fetch(state)
 	print(f'\tClock={state.clock} IR={hex(state.IR)} PC={hex(state.PC)}')	
 	if(state.IR==0):
 		break
-	state = proc.decode(state)
-	state = proc.execute(state)
-	state = proc.memory_access(state)
-	state = proc.write_back(state)
+	ID_EX = proc.decode(IF_ID)
+	EX_MEM = proc.execute(ID_EX)
+	MEM_WB = proc.memory_access(EX_MEM)
+	state = proc.write_back(MEM_WB)
+	
 print(f'Total number of clock cycles used up ={state.clock}')
 
 print('Final Memory:')
